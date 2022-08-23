3fcdd5d99a563d9e934d0a67847119e43329e0f0
Arek <arek.stachyra41@gmail.com>
2022-08-23 17:04:25 +0200

    Update file structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
-        },
-        open: true,
-        hot: true,
-    },
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Todo App',
-            filename: 'index.html',
-        }),
-    ],
+	mode: 'development',
+	devtool: 'inline-source-map',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		open: true,
+		hot: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Todo App',
+			filename: 'index.html',
+		}),
+	],
 };
