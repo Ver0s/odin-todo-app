e483f54f32f267d467dfb9326dd6b0e99d52d063
Arek <arek.stachyra41@gmail.com>
2022-09-27 15:56:54 +0200

    add event listeners to dynamically generated elements

--- src/modules/dom_creation/todo-dom.js
+++ src/modules/dom_creation/todo-dom.js
@@ -1,41 +1,66 @@
-import { showElement, hideElement } from './dom-utils';
-import { handleAddTodo, handleDeleteTodo } from '../app_logic/todo';
-import { renderTodo } from './elementTemplates';
+import { showElement, hideElement, showModal, hideModal } from './dom-utils';
+import {
+	handleAddTodo,
+	handleDeleteTodo,
+	handleEditTodo,
+} from '../app_logic/todo';
+import { renderTodo, addTodoForm, editTodoForm } from './elementTemplates';
 import { projectManager } from '../app_logic/projectManager';
 
 // SELECTORS
 const addTodoBtn = document.querySelector('#add-todo-btn');
-const addTodoModal = document.querySelector('.todo-modal');
-const cancelTodoForm = document.querySelector('#cancel-todo-form-btn');
-const addTodoForm = document.querySelector('#add-todo-form');
+// const cancelTodoForm = document.querySelector('#cancel-todo-form-btn');
+// const addTodoForm = document.querySelector('#add-todo-form');
 
 // FUNCTIONS
 const addTodoToDOM = (todo) => {
 	const todoList = document.querySelector('.todo-list');
-	const todoElement = renderTodo(todo);
-	todoList.appendChild(todoElement);
+	todoList.appendChild(renderTodo(todo));
 };
 
 // EVENT LISTENERS
 addTodoBtn.addEventListener('click', () => {
-	showElement(addTodoModal);
+	showModal(addTodoForm());
 });
 
-cancelTodoForm.addEventListener('click', () => {
-	hideElement(addTodoModal);
-});
-
-addTodoForm.addEventListener('submit', (e) => {
+document.addEventListener('submit', (e) => {
 	e.preventDefault();
-	handleAddTodo(e.target, projectManager.currentProject);
-	hideElement(addTodoModal);
+	const target = e.target;
+	if (target.id === 'add-todo-form') {
+		handleAddTodo(target, projectManager.currentProject);
+		hideModal();
+	}
+	if (target.id === 'edit-todo-form') {
+		hideModal();
+	}
 });
+
+document.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target.id === 'cancel-todo-form-btn') {
+		hideModal();
+	}
+});
+
+// cancelTodoForm.addEventListener('click', () => {
+// 	hideElement(addTodoModal);
+// });
+
+// addTodoForm.addEventListener('submit', (e) => {
+// 	e.preventDefault();
+// 	handleAddTodo(e.target, projectManager.currentProject);
+// 	hideElement(addTodoModal);
+// });
 
 document.addEventListener('click', (e) => {
 	const target = e.target;
 	if (target.dataset.todoAction === 'delete') {
 		const todoID = target.closest('.todo-item').dataset.todoId;
 		handleDeleteTodo(todoID, projectManager.currentProject);
+	}
+	if (target.dataset.todoAction === 'edit') {
+		const todoID = target.closest('.todo-item').dataset.todoId;
+		showModal(editTodoForm(projectManager.currentProject.getTodo(todoID)));
 	}
 });
 
