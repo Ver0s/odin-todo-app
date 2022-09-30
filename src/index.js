8193785d04dd541b57fe4cb0663369fd21ca0913
Arek <arek.stachyra41@gmail.com>
2022-09-30 13:49:37 +0200

    seperate app initialization and creating default project

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import './styles/style.css';
 import './styles/reset.css';
-import './modules/dom_creation/todo-dom';
-import './modules/dom_creation/sidebar-dom';
-import { setProjectHeader } from './modules/dom_creation/project-dom';
+import { handleProjectsNavigation } from './modules/dom_creation/sidebar-dom';
+import { renderProjects } from './modules/dom_creation/project-dom';
 import { Project } from './modules/app_logic/project';
-import { projectManager } from './modules/app_logic/projectManager';
-import { renderProjectLi } from './modules/dom_creation/elementTemplates';
+import { projectManager } from './modules/app_logic/project-manager';
 
+const setupDefaultProject = (projectName) => {
+	const defaultProject = Project(projectName);
+	const defaultProjectLi = document.querySelector('#default-project');
+	defaultProjectLi.setAttribute('data-project-id', defaultProject.id);
+	defaultProjectLi.textContent = defaultProject.title;
+	projectManager.addProject(defaultProject);
+	handleProjectsNavigation(defaultProject.id);
+};
+
+// Create initial project and add it to upper navigation + set header name
 const initApp = (() => {
-	// Create initial project and add it to upper navigation + set header name
-	const mainFilters = document.querySelector('.main-filters');
-	const homeProject = Project('Home');
-	const projectLiElement = renderProjectLi(homeProject);
-	mainFilters.insertBefore(projectLiElement, mainFilters.firstChild);
-	projectManager.addProject(homeProject);
-	projectManager.currentProject = homeProject;
-	setProjectHeader(projectManager.currentProject.title);
-	// add code for initial storage here
+	setupDefaultProject('Home');
+	renderProjects(projectManager.projects);
 })();
