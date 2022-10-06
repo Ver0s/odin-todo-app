4aa1b187f484c045bce7c6780b3ce57ec0cbb87e
Arek <arek.stachyra41@gmail.com>
2022-10-06 18:38:33 +0200

    adjust setupDefaultProject function to work with localstorage

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { Project } from './modules/app_logic/project';
 import { projectManager } from './modules/app_logic/project-manager';
 import { checkForStorage } from './modules/app_logic/storage';
 
-const setupDefaultProject = (projectName) => {
-	const defaultProject = Project(projectName);
+const setupDefaultProject = (defaultProject) => {
 	const defaultProjectLi = document.querySelector('#default-project');
 	defaultProjectLi.setAttribute('data-project-id', defaultProject.id);
 	defaultProjectLi.textContent = defaultProject.title;
@@ -15,10 +14,8 @@ const setupDefaultProject = (projectName) => {
 	handleProjectsNavigation(defaultProject.id);
 };
 
-// Create initial project and add it to upper navigation + set header name
 const initApp = (() => {
 	checkForStorage('projects');
-	// setupDefaultProject('Home');
 	renderProjects(projectManager.projects);
 })();
 
