57da5a1f20e667cf6e1ea6c3e701792ce6d22161
Arek <arek.stachyra41@gmail.com>
2022-10-06 13:12:00 +0200

    setup sidebar event listeners

--- src/modules/dom_creation/sidebar-dom.js
+++ src/modules/dom_creation/sidebar-dom.js
@@ -4,6 +4,7 @@ import { renderTodos } from './todo-dom';
 import { projectManager } from '../app_logic/project-manager';
 import { addProjectForm } from './element-templates';
 import { attachEventLitenersToAddProjectForm } from './event-listeners';
+import { getThisWeekTodos, getTodayTodos } from '../app_logic/logic-utils';
 
 // ELEMENTS FROM HTML TEMPLATE
 const toggleSidebarBtn = document.querySelector('#toggle-sidebar-btn');
@@ -20,14 +21,26 @@ const handleProjectsNavigation = (projectID) => {
 };
 
 // EVENT LISTENERS
+toggleSidebarBtn.addEventListener('click', () => {
+	const sidebar = document.querySelector('.sidebar');
+	sidebar.classList.toggle('hidden');
+});
+
 defaultProjectLi.addEventListener('click', () => {
 	const projectID = defaultProjectLi.dataset.projectId;
 	handleProjectsNavigation(projectID);
 });
 
-toggleSidebarBtn.addEventListener('click', () => {
-	const sidebar = document.querySelector('.sidebar');
-	sidebar.classList.toggle('hidden');
+todayTodosBtn.addEventListener('click', () => {
+	setProjectHeader('Today');
+	const todayTodos = getTodayTodos(projectManager.allProjectsTodos);
+	renderTodos(todayTodos);
+});
+
+thisWeekTodosBtn.addEventListener('click', () => {
+	setProjectHeader('This Week');
+	const thisWeekTodos = getThisWeekTodos(projectManager.allProjectsTodos);
+	renderTodos(thisWeekTodos);
 });
 
 addProjectBtn.addEventListener('click', () => {
