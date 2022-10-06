3d880347b86b5de98f6d350180bb6e0db794c14c
Arek <arek.stachyra41@gmail.com>
2022-10-06 13:16:01 +0200

    add date managing functionality

--- src/modules/app_logic/logic-utils.js
+++ src/modules/app_logic/logic-utils.js
@@ -1,6 +1,30 @@
+import isToday from 'date-fns/isToday';
+import isThisWeek from 'date-fns/isThisWeek';
+import parseISO from 'date-fns/parseISO';
+import endOfDay from 'date-fns/endOfDay';
+import isPast from 'date-fns/isPast';
+
+// const today = new Date(Date.now());
+// console.log(today.toDateString())
+
+const getTodayTodos = (todos) => {
+	return todos.filter((todo) => isToday(parseISO(todo.dueDate)));
+};
+
+const getThisWeekTodos = (todos) => {
+	return todos.filter((todo) => isThisWeek(parseISO(todo.dueDate)));
+	// isThisWeek(parseISO(todo.dueDate)) && !isPast(endOfDay(parseISO(todo.dueDate)))
+};
+
+// it can either be past today as a whole
+// or can be past today as in current hour
+const isTodoInPast = (todoDate) => {
+	return isPast(endOfDay(parseISO(todoDate)));
+};
+
 const getFormData = (form) => {
 	const formData = new FormData(form);
 	return Object.fromEntries(formData.entries());
 };
 
-export { getFormData };
+export { getFormData, getTodayTodos, getThisWeekTodos, isTodoInPast };
